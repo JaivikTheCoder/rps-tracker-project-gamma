0d31bb96edb9642ef874beec3ee64cc465d62bae
JaivikTheCoder <122547996+JaivikTheCoder@users.noreply.github.com>
2023-01-30 11:16:27 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+function showscoreboard () {
+    OLED.clear()
+    OLED.writeStringNewLine("PlayerA:" + PA)
+    OLED.newLine()
+    OLED.writeStringNewLine("PlayerB:" + PB)
+    OLED.newLine()
+    OLED.writeStringNewLine("Ties" + TIES)
+    OLED.newLine()
+    OLED.writeStringNewLine("Rounds" + ROUNDS)
+}
+let ROUNDS = 0
+let TIES = 0
+let PB = 0
+let PA = 0
+OLED.init(128, 64)
+PA = 0
+PB = 0
+TIES = 0
+ROUNDS = 0
+OLED.writeStringNewLine("Shall we play a game, babboon?")
+basic.pause(2000)
+showscoreboard()
