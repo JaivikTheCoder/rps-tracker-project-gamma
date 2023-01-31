f28b53e6a052f5762e2f4d504ad73c7d99be0c3e
JaivikTheCoder <122547996+JaivikTheCoder@users.noreply.github.com>
2023-01-31 11:28:45 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,23 +9,38 @@ function showscoreboard () {
     OLED.writeStringNewLine("Rounds" + ROUNDS)
 }
 input.onButtonPressed(Button.A, function () {
-    basic.showLeds(`
-        . . . . .
-        # . . # .
-        # . . # .
-        # # . . #
-        . . # . .
-        `)
+    PA += 1
+    ROUNDS += 1
+    showscoreboard()
+    basic.showString("A")
 })
+input.onButtonPressed(Button.AB, function () {
+    TIES += 1
+    ROUNDS += 1
+    showscoreboard()
+    basic.showString("T")
+})
+input.onButtonPressed(Button.B, function () {
+    PB += 1
+    ROUNDS += 1
+    showscoreboard()
+    basic.showString("B")
+})
+input.onGesture(Gesture.Shake, function () {
+    reset()
+})
+function reset () {
+    OLED.init(128, 64)
+    PA = 0
+    PB = 0
+    TIES = 0
+    ROUNDS = 0
+    OLED.writeStringNewLine("Shall we play a game, babboon?")
+    basic.pause(5000)
+    showscoreboard()
+}
 let ROUNDS = 0
 let TIES = 0
 let PB = 0
 let PA = 0
-OLED.init(128, 64)
-PA = 0
-PB = 0
-TIES = 0
-ROUNDS = 0
-OLED.writeStringNewLine("Shall we play a game, babboon?")
-basic.pause(5000)
-showscoreboard()
+reset()
